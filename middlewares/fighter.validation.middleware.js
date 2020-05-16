76c8af0d6523e04b03ec0c43f1e78ccb4f0a9d89
Denis Karpov <karpov.syned@gmail.com>
2020-05-16 22:21:23 +0300

    feat: add service, routes, validation for fighter

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,13 +1,76 @@
 const { fighter } = require('../models/fighter');
+const FighterService = require('../services/fighterService');
+const { fieldsValidation, fighterExists } = require('../validation');
+
+const fighterSchema = Object.assign({}, fighter);
+delete fighterSchema.id;
 
 const createFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for fighter entity during creation
-  next();
+  const newFighter = req.body;
+  try {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!Object.keys(fighterSchema).every(key => newFighter.hasOwnProperty(key))) {
+      return res.status(400).send({
+        error: true,
+        message: 'All fields must be filled in.'
+      });
+    }
+
+    const errors = fieldsValidation(FighterService, req.body);
+
+    if (errors.length) {
+      return res.status(400).send({
+        error: true,
+        message: errors
+      });
+    }
+
+    const { name, power, health, defense } = newFighter;
+    req.newFighter = { name, power, health, defense };
+    next();
+  } catch (e) {
+    res.status(500).send({ // TODO move to error handler middleware
+      error: true,
+      message: 'Internal server error'
+    });
+  }
 };
 
 const updateFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for fighter entity during update
-  next();
+  try {
+    if (!fighterExists(FighterService, { id: req.params.id })) {
+      return res.status(404).send({
+        error: true,
+        message: 'Fighter not found'
+      });
+    }
+
+    const allowedUpdates = Object.keys(fighterSchema);
+    const reqUpdates = Object.keys(req.body);
+    const isValidUpdate = reqUpdates.every(field => allowedUpdates.includes(field));
+
+    if (!isValidUpdate) {
+      return res.status(400).send({
+        error: true,
+        message: 'Invalid updates'
+      });
+    }
+
+    const errors = fieldsValidation(FighterService, req.body);
+
+    if (errors.length) {
+      return res.status(400).send({
+        error: true,
+        message: errors
+      });
+    }
+    next();
+  } catch (e) {
+    res.status(500).send({ // TODO move to error handler middleware
+      error: true,
+      message: 'Internal server error'
+    });
+  }
 };
 
 exports.createFighterValid = createFighterValid;
--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -6,6 +6,114 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 // eslint-disable-next-line new-cap
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+// @route GET /api/fighters
+// @desc Returns all fighters in db
+router.get('/', (req, res) => {
+  try {
+    const fighters = FighterService.getAll();
+
+    if (!fighters) {
+      res.status(404).send({
+        error: true,
+        message: 'Have no fighters'
+      });
+    }
+    res.send(fighters);
+  } catch (e) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error'
+    });
+  }
+});
+
+// @route GET /api/fighters/:id
+// @desc Returns specific fighter by id
+router.get('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const foundFighter = FighterService.search({ id });
+
+    if (!foundFighter) {
+      res.status(404).send({
+        error: true,
+        message: 'No fighter with such id'
+      });
+    }
+    res.send(foundFighter);
+  } catch (e) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error'
+    });
+  }
+});
+
+// @route POST /api/fighters
+// @desc Creates fighter
+router.post('/', createFighterValid, (req, res) => {
+  const fighter = req.newFighter;
+  try {
+    const result = FighterService.create(fighter);
+
+    if (!result) {
+      res.status(400).send({
+        error: true,
+        message: 'Non validation error'
+      });
+    }
+    res.send(result);
+  } catch (e) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error'
+    });
+  }
+});
+
+// @route PUT /api/fighters/:id
+// @desc Changes fighter details by id
+router.put('/:id', updateFighterValid, (req, res) => {
+  const { id } = req.params;
+  const dataToUpdate = req.body;
+  try {
+    const updatedFighter = FighterService.update(id, dataToUpdate);
+
+    if (!updatedFighter) {
+      res.status(404).json({
+        error: true,
+        message: 'No fighter with such id'
+      });
+    }
+    res.send(updatedFighter);
+  } catch (e) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error'
+    });
+  }
+});
+
+// @route DELETE /api/fighters/:id
+// @desc Removes fighter by id
+router.delete('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const fighter = FighterService.delete(id);
+
+    if (!fighter) {
+      res.status(404).send({
+        error: true,
+        message: 'Fighter not found'
+      });
+    }
+    res.send(fighter);
+  } catch (e) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error'
+    });
+  }
+});
 
 module.exports = router;
